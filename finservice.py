7b14ea03dd5ffb36db1e367aac950ff37e5385b2
Amani, Fredrick F <fredrick.amani@stanbic.co.tz>
2023-01-23 13:46:55 +0300

    Initial commit

---
+++ finservice.py
@@ -0,0 +1,12 @@
+import os
+
+def run_job():
+    os.system("ls -l")
+
+
+def main():
+    run_job()
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+
+def run_job():
+    time.sleep(3)
+    os.system("dir")
+
+
+if __name__ == '__main__':
+    run_job()
